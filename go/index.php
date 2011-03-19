6ee2392f65763a132ef823c2149553c59c2cdf17
james <james@192.168.1.2>
2011-03-19 19:22:26 +0800

    added plugin.php support to: + overwrite file alias name + to change url result after retrieval

--- go/index.php
+++ go/index.php
@@ -4,11 +4,22 @@ $sPath = dirname(dirname(__FILE__)) . "/ip/config.";
 
 $sName = $_REQUEST["name"];
 $sName = preg_replace("/[^a-zA-Z0-9_\-]/", "", $sName);
-$sFile = $sPath . $sName . ".php";
+$sAlias = $sName;
 
+if (file_exists(dirname(__FILE__)."/plugin.php"))  require_once dirname(__FILE__).'/plugin.php';
+//getconfig file, return alias if possible
+if (function_exists("onGetAlias")) {
+  $sAlias = onGetAlias($sName);
+}
+
+$sFile = $sPath . $sAlias . ".php";
 if (file_exists($sFile)) {
   require_once($sFile);
   $sURL = "http://" . $ip;
+
+  if (function_exists("onGetURL")) {
+    onGetURL($sName, $ip, $sURL);
+  }
   //you may alter url based on name here
   header("location: " . $sURL);
 } else {
